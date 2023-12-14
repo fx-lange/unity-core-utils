453fdb79f16a97864a5004528ae18ce5637883e7
Felix Lange <mail@felixlange.dev>
2023-12-14 17:03:59 +0100

    simple generic state class

---
+++ Runtime/State.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace CoreFx
+{
+    [Serializable]
+    public class State<T>
+    {
+        [SerializeField] private T _value;
+
+        public T Value
+        {
+            get => _value;
+            private set => _value = value;
+        }
+
+        public event Action<T> ValueChangedEvent;
+
+        public void SetValue(T value, bool silent = false)
+        {
+            Value = value;
+
+            if (silent) return;
+
+            ValueChangedEvent?.Invoke(Value);
+        }
+
+        public static implicit operator T(State<T> state) => state.Value;
+    }
+}
