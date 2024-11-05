bd318c4bd3b64d0037c0126a25d34bd3809c64a1
Felix Lange <mail@felixlange.dev>
2024-11-05 14:51:58 +0100

    quick fix for unity 6 domain reload

--- Editor/SceneSelector/SceneSelectorWindow.cs
+++ Editor/SceneSelector/SceneSelectorWindow.cs
@@ -125,9 +125,9 @@ namespace SceneSelector
 
         private void OnDisable()
         {
-            _input.UnregisterValueChangedCallback(UpdateKeyword);
-            _input.UnregisterCallback<FocusOutEvent>(CallPopulate);
-            _objectField.UnregisterValueChangedCallback(SceneChangeCallback);
+            _input?.UnregisterValueChangedCallback(UpdateKeyword);
+            _input?.UnregisterCallback<FocusOutEvent>(CallPopulate);
+            _objectField?.UnregisterValueChangedCallback(SceneChangeCallback);
         }
 
         VisualElement CreateSceneButton(SceneAsset sceneAsset)
