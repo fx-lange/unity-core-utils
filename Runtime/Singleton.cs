eef8d4dde1233b2e4e936d29036a8526c1b36b32
Felix Lange <mail@felixlange.dev>
2025-01-10 10:48:19 +0100

    add stricly typed service locator and singleton

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CoreFx
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Runtime/StrictServiceLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace CoreFx
+{
+    public interface IService
+    {
+    }
+
+    //service locator without interface abstraction
+    public static class StrictServiceLocator
+    {
+        private static Dictionary<Type, IService> _services = new();
+
+        public static void Register(IService service)
+        {
+            var type = service.GetType();
+            if (!_services.TryAdd(type, service))
+            {
+                Debug.LogWarning($"Type {type} already registered");
+            }
+        }
+
+        public static T Get<T>() where T : Object, IService
+        {
+            var type = typeof(T);
+            if (!Application.isPlaying)
+            {
+                return Object.FindAnyObjectByType<T>();
+            }
+            
+            if (_services.TryGetValue(type, out var service))
+            {
+                return service as T;
+            }
+
+            Debug.LogError($"No service of type {type}");
+            return null;
+        }
+    }
+}
